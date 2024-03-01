420abf9fb4e88b192ae2ff7e50d55ead7d27dad2
Elizabeth Gutierrez <elizabethgolvera@gmail.com>
2024-03-01 13:40:48 -0600

    Segunda - 01 de Marzo de 2024

--- dist/app.js
+++ dist/app.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+const usuarioRoutes_1 = __importDefault(require("./routes/usuarioRoutes"));
 /*
 * Clase de inicio de nuestra aplicación NodeJsExpress
-* Autor: Elizabeth Gutierrez Olvera
-* Fecha: 28 de febrero de 2024
+* Autor: Gabriel Barrón Rodríguez
+* Fecha: 23 Junio 2024
 */
 class Server {
     //Inicializa clase
@@ -24,18 +26,20 @@ class Server {
     }
     //Configuración de módulos
     config() {
-        //configuración del puerto para el servidor
+        // configuración del puerto para el servidor
         this.app.set("port", 3000);
-        //muestra las peticiones en consola
+        // muestra las peticiones en consola
         this.app.use((0, morgan_1.default)("dev"));
-        //Puertos de conexion de la API
+        // puertos de conexión de la API
         this.app.use((0, cors_1.default)());
-        //Solo se permiten peticiones en formato JSON
+        // solo se permiten peticiones en formato JSON
         this.app.use(body_parser_1.default.json());
-        this.app.use(body_parser_1.default.urlencoded({ extended: false }));
+        this.app.use(body_parser_1.default.urlencoded({ extended: false, }));
     }
-    //Configuración de las rutas
+    //Configura las rutas
     routes() {
+        this.app.use("/", authRoutes_1.default);
+        this.app.use('/usuario', usuarioRoutes_1.default);
     }
 }
 const server = new Server();
